1a2cdbf677cf3a799c7b1fbfce3f3448f3101d20
Will Tekulve <tekulve.will@gmail.com>
2017-10-28 01:01:19 -0400

    Modify for new design

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@ from distutils.core import setup
 from setuptools import find_packages
 
 
-def get_requirements():
-    with open('requirements.txt') as f:
-        reqs = f.readlines()
-    return reqs
-
-
 setup(
     name='Red-Trivia',
     version='0.0.1',
@@ -15,6 +9,5 @@ setup(
     license='GPLv3',
     author='Cog-Creators',
     description='A collection of trivia lists curated by Red-DiscordBot developers',
-    install_requires=get_requirements(),
-    entry_points={'redbot.trivia': '.core = trivia'}
+    entry_points={'redbot.trivia': 'lists = trivia.lists'}
 )
--- trivia/__init__.py
+++ trivia/__init__.py
@@ -1,35 +1,8 @@
 from pathlib import Path
-from typing import List
 
-__all__ = ['list_names']
-
-_list_map = None
+__all__ = ['lists']
 
 
-def _lists_dir() -> Path:
-    return Path(__file__).parent.resolve() / 'lists'
-
-
-def _load_list_names():
-    lists_dir = _lists_dir()
-
-    lists_paths = lists_dir.glob("*.yaml")
-
-    global _list_map
-    _list_map = {p.stem: p.resolve() for p in lists_paths}
-
-
-def list_map() -> dict:
-    """
-    Enumerates the trivia lists available from this repo.
-
-    Returns
-    -------
-    list of str
-        List of trivia names.
-    """
-    if _list_map is None:
-        _load_list_names()
-
-    # noinspection PyTypeChecker
-    return _list_map
+def lists():
+    lists_dir = Path(__file__).parent.resolve() / 'lists'
+    return lists_dir.glob("*.yaml")
