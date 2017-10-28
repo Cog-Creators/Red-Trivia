baf5c2cfee842a96f840531cf5cc262b4f808ae8
Will Tekulve <tekulve.will@gmail.com>
2017-10-28 01:31:43 -0400

    Make everything into namespaced package

--- redbot/trivia/__init__.py
+++ redbot/trivia/__init__.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
 
 __all__ = ['lists']
 
--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from distutils.core import setup
-from setuptools import find_packages
 
 
 setup(
     name='Red-Trivia',
     version='0.0.1',
-    packages=find_packages(),
+    namespace_packages=['redbot'],
+    packages=['redbot.trivia'],
     license='GPLv3',
     author='Cog-Creators',
-    description='A collection of trivia lists curated by Red-DiscordBot developers',
-    entry_points={'redbot.trivia': 'lists = trivia.lists'}
+    description='A collection of trivia lists curated by Red-DiscordBot developers'
 )
