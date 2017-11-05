a193a2fcc08fbe4952b730b1edefc7e988d084b4
Will Tekulve <tekulve.will@gmail.com>
2017-11-05 18:39:50 -0500

    Add package data

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 from distutils.core import setup
+from pathlib import Path
+
+
+def get_lists():
+    base_dir = Path('redbot/trivia/lists')
+    parents = base_dir.parents
+    return [str(p.relative_to(parents[0])) for p in base_dir.glob("*.yaml")]
 
 
 setup(
     name='Red-Trivia',
-    version='0.0.1',
+    version='0.0.2',
     namespace_packages=['redbot'],
     packages=['redbot.trivia'],
+    package_data={'redbot.trivia': get_lists()},
     license='GPLv3',
     author='Cog-Creators',
-    description='A collection of trivia lists curated by Red-DiscordBot developers',
-    include_package_data=True
+    description='A collection of trivia lists curated by Red-DiscordBot developers'
 )
